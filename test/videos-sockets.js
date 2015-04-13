71f48304079dc35db53af0518e0005b46e86d9ec
Gean Carlos <w4ch4p@gmail.com>
2015-04-13 12:42:31 -0500

    cambios sockets

--- test/videos-sockets.js
+++ test/videos-sockets.js
@@ -19,13 +19,40 @@ describe('Probando sockets [/videos]', function () {
 
 		client.on("testServer", function (data) {
 			console.log(data);
-
 			client.emit('testCliente', {hola:'mundo desde el cliente'});
 
 			client.disconnect();
 			done();
 		});
 
+		// client.on("testCliente", function (data) {
+		// 	console.log(data);
+
+		// });
 	});
 
+	// User sends a video to everyone
+	// it("deberia ser capaz de emitir videos", function (done) {
+	// 	var client1, client2, client3;
+	// 	var data = {
+	// 		"video": {
+	// 			"id": "TcnQdb8BYcY",
+	// 			"url": "https://www.youtube.com/watch?v=TcnQdb8BYcY",
+	// 			"title": "Ed Sheeran - Thinking Out Loud HD (Sub español - ingles)",
+	// 			"iurlsd": "https://i.ytimg.com/vi/TcnQdb8BYcY/sddefault.jpg",
+	// 			"thumbnail_url": "https://i.ytimg.com/vi/TcnQdb8BYcY/default.jpg"
+	// 		}
+	// 	};
+
+	// 	var checkVideo = function (client) {
+	// 		client.on('newVideo', function (data) {
+	// 			done()
+	// 		});
+	// 	};
+
+
+	// });
+
 });
+
+// https://github.com/liamks/Testing-Socket.IO/blob/master/test/test-chat-server.js
