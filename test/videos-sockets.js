9b4a3f4b34e32d846626b7c3cdf90bdf8bf23a61
wochap <w4ch4pgmail.com>
2015-04-16 18:57:54 -0500

    añadiendo realtime

--- test/videos-sockets.js
+++ test/videos-sockets.js
@@ -1,8 +1,6 @@
 var os = require("os");
 var request = require('supertest-as-promised');
-var api = require('../server.js');
-// var host = process.env.API_TEST_HOST || api;
-var host = 'http://localhost:3000'
+var host = process.env.API_TEST_HOST || 'http://localhost:3000';
 var io = require('socket.io-client');
 
 
